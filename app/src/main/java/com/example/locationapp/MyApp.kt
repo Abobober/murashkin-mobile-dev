a016669cd61ea12a54b742a97e6a5ad2ce7d166c
user <murashkin_d@iuca.kg>
2024-12-12 12:07:35 +0600

    Location App added

---
+++ app/src/main/java/com/example/locationapp/MyApp.kt
@@ -0,0 +1,12 @@
+package com.example.locationapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+@Composable
+fun MyApp(viewModel: LocationViewModel) {
+    val context = LocalContext.current
+    val locationUtils = LocationUtils(context)
+
+    LocationDisplay(locationUtils = locationUtils, context = context, viewModel)
+}
